981f4bc99635ee04f25c7cbb630ea81f797d4dd0
Shobhit <shobhita@andrew.cmu.edu>
2024-10-29 11:06:02 -0400

    update with custom msgs

--- run_docker.sh
+++ run_docker.sh
@@ -13,4 +13,7 @@ docker run -it --rm --net=host \
   -v "$(pwd)/rosbridge_server:${CATKIN_DIR}/src/rosbridge_server:ro" \
   -v "$(pwd)/rosbridge_msgs:${CATKIN_DIR}/src/rosbridge_msgs:ro" \
   -v "$(pwd)/rosapi:${CATKIN_DIR}/src/rosapi:ro" \
+  -v "/home/mfi/repos/ros1_ws/src/shobhit/motoman_ros1/depend-packages/industrial_core/industrial_msgs:${CATKIN_DIR}/src/industrial_msgs:ro"\
+  -e ROS_MASTER_URI=http://192.168.1.2:11311 \
+  -e ROS_IP=192.168.1.2 \
   rosbridge_dev $@
